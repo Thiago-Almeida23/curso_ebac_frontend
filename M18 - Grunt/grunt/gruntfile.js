4207cbdf2df4eff8c674a970ce36f343e71eeecc
Thiago-Almeida23 <thiagocldealmeida@gmail.com>
2024-03-04 07:04:41 -0300

    Fazendo commit da tarefa do Módulo 18.

---
+++ M18 - Grunt/grunt/gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'main.min.js': 'main.js'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('compile-less', ['less:development', 'less:production']);
+    grunt.registerTask('compress-js', ['uglify:target']);
+}
